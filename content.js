9b50eca1a3ffd04ca9509283ce0c65ceace34043
damo989 <damo989@iinet.net.au>
2021-08-28 18:08:10 +0800

    Added fetch stats for each video on homepage

--- content.js
+++ content.js
@@ -1,6 +1,7 @@
 
 if( document.readyState !== 'loading' ) {
     myInitCode();
+
 } else {
     document.addEventListener('DOMContentLoaded', function () {
         myInitCode();
@@ -8,8 +9,35 @@ if( document.readyState !== 'loading' ) {
 }
 
 function myInitCode() {
+    console.log("Page Loaded");
+    getAllLinks();
+}
+
+function getAllLinks(){
+    links = document.querySelectorAll("#video-title-link")
+    for (i=0; i < links.length; i++){
+        linkID = links[i].href.split("v=")[1];
+        fetchStats(linkID)
+        
+    }
     
 }
+
+
+
+async function fetchStats(id){
+    const response = await fetch("https://youtube.googleapis.com/youtube/v3/videos?part=statistics&id=" + id + "&key=AIzaSyCZ0Nc68EsVvYNKeOeRsVGxnSehYBus3Xc")
+    const responseJ = await response.json()
+    const stats = responseJ.items[0].statistics
+    let rating = Math.floor(Number(stats.likeCount) / (Number(stats.likeCount) + Number(stats.dislikeCount)) * 100);
+    console.log("Likes: " + stats.likeCount);
+    console.log("Dislikes:" + stats.dislikeCount);
+    console.log("Rating: " + rating);
+}
+
+
+
+
 
 
 
