a63dbfcd12ff3e8079d12c1d033ca296f0e5a16b
damienbrace <damienbrace@gmail.com>
2021-08-30 18:05:39 +0800

    added stylesheet

--- content.js
+++ content.js
@@ -17,7 +17,8 @@ function getAllLinks(){
     links = document.querySelectorAll("#video-title-link")
     for (i=0; i < links.length; i++){
         linkID = links[i].href.split("v=")[1];
-        fetchStats(linkID)
+        linkTitle = links[i].title;
+        fetchStats(linkID, linkTitle)
         
     }
     
@@ -25,16 +26,24 @@ function getAllLinks(){
 
 
 
-async function fetchStats(id){
+async function fetchStats(id, title){
     const response = await fetch("https://youtube.googleapis.com/youtube/v3/videos?part=statistics&id=" + id + "&key=AIzaSyCZ0Nc68EsVvYNKeOeRsVGxnSehYBus3Xc")
     const responseJ = await response.json()
     const stats = responseJ.items[0].statistics
     let rating = Math.floor(Number(stats.likeCount) / (Number(stats.likeCount) + Number(stats.dislikeCount)) * 100);
+
+    
+    console.log("Title: "+ title);
+    console.log("ID: "+ id);
     console.log("Likes: " + stats.likeCount);
     console.log("Dislikes:" + stats.dislikeCount);
     console.log("Rating: " + rating);
+    console.log(" ");
 }
 
+function addRatingsToPage(){
+    
+}
 
 
 
--- fetch.js
+++ fetch.js
@@ -1,12 +1,7 @@
 async  function fetchYoutube(){
     const response = await fetch('https://www.youtube.com/watch?v=cuEtnrL9-H0');
     const html = await response.text();
-<<<<<<< HEAD
     return html;
-=======
-    console.log(html);
- 
->>>>>>> 9b50eca1a3ffd04ca9509283ce0c65ceace34043
 }
 
 function convertToHTML(text){
@@ -15,11 +10,6 @@ function convertToHTML(text){
     return doc;
 }
 
-<<<<<<< HEAD
 let html = await fetchYoutube();
 let htmlDoc = await convertToHTML(html);
 console.log(htmlDoc);
-=======
-fetchYoutube();
-
->>>>>>> 9b50eca1a3ffd04ca9509283ce0c65ceace34043
