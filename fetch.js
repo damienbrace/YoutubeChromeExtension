a3e6149c6b5d30b08fd3b2efe6c8beedb63e5b45
damienbrace <damienbrace@gmail.com>
2021-08-30 16:59:46 +0800

    k

--- fetch.js
+++ fetch.js
@@ -2,10 +2,14 @@ async  function fetchYoutube(){
     const response = await fetch('https://www.youtube.com/watch?v=cuEtnrL9-H0');
     const html = await response.text();
     return html;
- 
 }
 
-
+function convertToHTML(text){
+    let parser = new DOMParser();
+    let doc = parser.parseFromString(text, 'text/html');
+    return doc;
+}
 
 let html = await fetchYoutube();
-console.log(html);
+let htmlDoc = await convertToHTML(html);
+console.log(htmlDoc);
